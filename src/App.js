c6726a57a564b0e1c4ab9bb34bdfdc4dd1717d83
hoangFE <metmoi556@email.com>
2022-08-23 22:14:40 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import FormValidation from './Form_validation/FormValidation'
+
+function App() {
+  return (
+    <div className="App">
+      <FormValidation/>
+    </div>
+  );
+}
+
+export default App;
